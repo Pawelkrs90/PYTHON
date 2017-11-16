c5b6309b872a6268407a07c4df7bb3d6dd9d869a
Pawelkrs90 <Pawelkrs@hotmail.com>
2017-11-16 18:31:11 +0100

    zajecia powt2

---
+++ testKlasy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+
+class klasa1:
+
+	__init__(self):
+		a = "aaa"
+		b = "bbb"
+
+class klasa2(Object):
+		
+	__init__(self):
+		a = "aaa"
+		b = "bbb"
+
+
+#klasa encyjna dziedziczy po models.Model
+
+from django.db import models
+class testClass(models.Model)
